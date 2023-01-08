bf5cc53db5763db944581561bd906847e85a4fdd
Koen Zomers <koen@zomers.eu>
2023-01-08 22:32:24 +0100

    Bugfixes

---
+++ _posts/2022-12-13-organisatie-van-start.md
@@ -0,0 +1,10 @@
+---
+title: "Organisatie van start"
+last_modified_at: 2023-01-08T03:05:00+01:00
+categories:
+  - Organisatie
+tags:
+  - 60e editie
+---
+
+De organisatie is begonnen
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,4 +1,10 @@
 var store = [{
+        "title": "Organisatie van start",
+        "excerpt":"De organisatie is begonnen  ","categories": ["Organisatie"],
+        "tags": ["60e editie"],
+        "url": "/organisatie/organisatie-van-start/",
+        "teaser": null
+      },{
         "title": "Nieuwe website",
         "excerpt":"Nieuwe website!  ","categories": ["Post Formats"],
         "tags": ["Post Formats","readability","standard"],
