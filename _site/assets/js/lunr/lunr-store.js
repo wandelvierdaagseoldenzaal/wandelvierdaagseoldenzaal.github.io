2ec3af829be2cb6612de8e51e63c7131dd361c23
Koen Zomers <koen@zomers.eu>
2023-01-20 21:58:45 +0100

    Changed button color to green

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,12 +2,12 @@ var store = [{
         "title": "Organisatie van start",
         "excerpt":"De organisatie van de zestigste editie van de Wandel4Daagse Oldenzaal is begonnen. De voorbereidingen zijn in volle gang. De routes worden zorgvuldig voorbereid en de inschrijvingen zullen binnenkort openen. De organisatie is in handen van Hennie Maartens, Dennis Eidhof, Gerald Voorpostel, Bennie Oude Maatman en Koen Zomers. Zij zullen de...","categories": ["nieuws"],
         "tags": ["60e editie"],
-        "url": "/nieuws/organisatie-van-start/",
+        "url": "/pages/koenzomers/nieuws/organisatie-van-start/",
         "teaser": null
       },{
         "title": "Nieuwe website",
         "excerpt":"Er is een nieuwe website ontwikkeld voor de Wandel4Daagse. De website is nu nog in ontwikkeling, maar de belangrijkste informatie is al beschikbaar. De komende tijd zullen er nog meer pagina’s worden toegevoegd. Heb je suggesties voor de website, laat het ons weten door contact op te nemen.  ","categories": ["nieuws"],
         "tags": ["website"],
-        "url": "/nieuws/nieuwe-website/",
+        "url": "/pages/koenzomers/nieuws/nieuwe-website/",
         "teaser": null
       }]
